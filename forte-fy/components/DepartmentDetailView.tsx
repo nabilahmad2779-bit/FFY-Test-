ddc53a9d497a6b38d2a681143f16e94bce67c561
nabilahmad2779-bit <nabil.ahmad.2779@gmail.com>
2026-02-01 04:00:30 +0600

    Add files via upload

---
+++ forte-fy/components/DepartmentDetailView.tsx
@@ -0,0 +1,37 @@
+
+import React from 'react';
+import HRDepartment from './departments/HRDepartment';
+import PRDepartment from './departments/PRDepartment';
+import ITDepartment from './departments/ITDepartment';
+import OpsDepartment from './departments/OpsDepartment';
+import AcadDepartment from './departments/AcadDepartment';
+
+interface DepartmentDetailViewProps {
+  dept: any;
+  navigate: (path: string) => void;
+  isDark: boolean;
+  setIsDark: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const DepartmentDetailView: React.FC<DepartmentDetailViewProps> = ({ dept, navigate, isDark, setIsDark }) => {
+  switch (dept.id) {
+    case 'hr':
+      return <HRDepartment dept={dept} navigate={navigate} isDark={isDark} setIsDark={setIsDark} />;
+    case 'pr':
+      return <PRDepartment dept={dept} navigate={navigate} isDark={isDark} setIsDark={setIsDark} />;
+    case 'it':
+      return <ITDepartment dept={dept} navigate={navigate} isDark={isDark} setIsDark={setIsDark} />;
+    case 'ops':
+      return <OpsDepartment dept={dept} navigate={navigate} isDark={isDark} setIsDark={setIsDark} />;
+    case 'acad':
+      return <AcadDepartment dept={dept} navigate={navigate} />;
+    default:
+      return (
+        <div className="min-h-screen flex items-center justify-center text-white">
+           <p>Department configuration not found.</p>
+        </div>
+      );
+  }
+};
+
+export default DepartmentDetailView;
