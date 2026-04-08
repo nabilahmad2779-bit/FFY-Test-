ff137dfb18029f2708a0cad0cfc5f808961569ad
nabilahmad2779-bit <nabil.ahmad.2779@gmail.com>
2026-04-08 18:40:42 +0600

    Delete forte-fy directory

--- forte-fy/components/MainMenuPages/MainMenuPagesLight/PanelPageLight.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const PanelPageLight: React.FC = () => {
-  return <div className="min-h-screen bg-[#f8f9fa]" />;
-};
--- forte-fy/components/MainMenuPages/MainMenuPagesMobile/MobileMenuLight/PanelPageMobileLight.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export const PanelPageMobileLight: React.FC = () => {
-  return <div className="min-h-screen bg-slate-50" />;
-};
-
